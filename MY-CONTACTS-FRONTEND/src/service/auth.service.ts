1422b9a35f435a846c9db8a122f9de50f3693372
Fvrenn <hegetimothe@gmail.com>
2025-10-09 10:18:35 +0200

    prod: back url

--- MY-CONTACTS-FRONTEND/src/service/auth.service.ts
+++ MY-CONTACTS-FRONTEND/src/service/auth.service.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/";
+const API_URL = "https://mycontacts-p9kf.onrender.com/";
 
 class AuthService {
   login(username: string, password: string) {
